06bf821263c5d4916a3b66c6615f81416d96f0e8
Lagrida <lagyassine@gmail.com>
2021-09-22 20:49:15 +0100

    Node Express Redis Docker project

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,7 @@
 const { getFromCache, resetRedis } = require("../helpers/redis");
 
 exports.indexHome = async (req, res, next) => {
+    console.log('inside index home');
     res.status(200).json({
         message: "Api index home"
     });
